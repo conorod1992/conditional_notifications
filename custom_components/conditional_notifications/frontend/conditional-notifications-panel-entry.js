30c53651eff672fdb970ff99f4f765e0a67bc426
conorod1992 <58264595+conorod1992@users.noreply.github.com>
2026-08-30 17:21:27 +0100

    Reject stale panel edits with record revisions

--- custom_components/conditional_notifications/frontend/conditional-notifications-panel-entry.js
+++ custom_components/conditional_notifications/frontend/conditional-notifications-panel-entry.js
@@ -12,4 +12,37 @@ panel.hydrateEditor = function() {
   if (helpers.length > 1) helpers[0].remove();
 };
 
+panel.save = async function() {
+  const definition = this.editor.definition;
+  this.errors = this.validate(definition);
+  if (Object.keys(this.errors).length) {
+    this.showToast("Check the highlighted fields");
+    this.render();
+    return;
+  }
+
+  try {
+    const editing = Boolean(this.editor.id);
+    if (editing) {
+      await this.hass.callWS({
+        type: "conditional_notifications/update",
+        notification_id: this.editor.id,
+        changes: definition,
+        expected_revision: this.editor.original?.revision,
+      });
+    } else {
+      await this.hass.callWS({
+        type: "conditional_notifications/create",
+        definition,
+      });
+    }
+    this.dirty = false;
+    this.closeEditor(true);
+    this.showToast(editing ? "Changes saved" : "Notification created");
+    await this.refresh();
+  } catch (error) {
+    this.showToast(error.message || String(error));
+  }
+};
+
 export { ConditionalNotificationsPanel };
